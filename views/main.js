a2d6b783f80cfeeffebae6c87489b07f615ec221
Allen Hai <allenhai03@gmail.com>
2018-09-19 05:06:53 -0500

    add red background for all elapsed hour blocks

--- views/main.js
+++ views/main.js
@@ -1,41 +1,30 @@
-var html = require('choo/html')
+const html = require('choo/html')
 
-var TITLE = `${new Date()}`
+let currentTime = new Date()
+let currentHour = currentTime.getHours() 
+const TITLE = String(currentTime)
 
 module.exports = view
 
 function view (state, emit) {
   if (state.title !== TITLE) emit(state.events.DOMTITLECHANGE, TITLE)
-
+  
   return html`
     <body class="code lh-copy">
       <main class="pa3 cf center">
         <div class="tc">${TITLE}</div>
         <div class="center flex flex-wrap justify-center w-two-thirds">
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">1</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">2</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">3</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">4</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">5</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">6</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">7</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">8</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">9</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">10</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">11</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">12</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">13</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">14</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">15</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">16</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">17</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">18</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">19</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">20</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">21</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">22</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">23</div>
-          <div class="ba b--near-black w3-ns h3-ns debug-grid ma3-ns">24</div>
+          ${Array(24).join(0).split(0).map((item, i) => {
+            if (i <= currentHour) { 
+              return html`
+                <div class="pa3 bg-red white w3 h3 ma3">${i}</div>
+              `
+            }
+            return html`
+              <div class="pa3 bg-black-70 white w3 h3 ma3">${i}</div>
+            `
+          })
+          }
         </div>
       </main>
     </body>
