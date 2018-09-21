d417284ed0fe7db45b9b9d9bb7ee0e689fc5f7e3
Allen Hai <allenhai03@gmail.com>
2018-09-20 22:05:01 -0500

    invert bg and text color for current hour block

--- views/main.js
+++ views/main.js
@@ -15,14 +15,19 @@ function view (state, emit) {
         <div class="tc">${TITLE}</div>
         <div class="center flex flex-wrap justify-center w-two-thirds">
           ${Array(24).join(0).split(0).map((item, i) => {
-            if (i <= currentHour) { 
+            if (i < currentHour) { 
               return html`
                 <div class="pa3 bg-red white w3 h3 ma3">${i}</div>
               `
+            } else if (i === currentHour) {
+                return html`
+                  <div class="pa3 red ba b--red bw2 w3 h3 ma3">${i}</div>
+                `
+            } else {
+              return html`
+                <div class="pa3 bg-black-70 white w3 h3 ma3">${i}</div>
+              `
             }
-            return html`
-              <div class="pa3 bg-black-70 white w3 h3 ma3">${i}</div>
-            `
           })
           }
         </div>
